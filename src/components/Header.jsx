63c089537494cbd12922149e1374e21f48bac362
Damiana-Arangio <arangio.damiana@gmail.com>
2025-11-04 01:22:30 +0100

    Milestone 1 + Milestone 2 + Bonus: - Installato Bootstrap e importato in main.jsx - Aggiornato componente Header con navbar Bootstrap, logo e titolo - Utilizzato Link di React Router per rendere logo e titolo cliccabili e reindirizzare alla HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 /************************** Componente Header ****************************/
 
+import { Link } from "react-router-dom";      /* Import componenti da React Router */
+
+
 function Header() {
     return(
-        <h1> Header </h1>
+
+        <nav className="navbar bg-light-subtle">
+            <div className="container-fluid">
+                <Link className="navbar-brand fs-1 fw-bold" to="/movies">
+                    <img src="../public/imgs/logo.jpg" alt="Logo" width="70" height="64" className="d-inline-block align-text-top me-3"/>
+                        Movies App
+                </Link>
+            </div>
+        </nav>
     )
 }
 
